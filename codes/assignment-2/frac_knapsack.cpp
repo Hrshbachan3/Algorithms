59ab39d55e40d456f2e1151cf73d0bc22f88c63b
Hrshbachan3 <hrshbachan3@gmail.com>
2023-02-10 03:13:40 +0530

    added knapsack algorithm

---
+++ codes/assignment-2/frac_knapsack.cpp
@@ -0,0 +1,58 @@
+// calculate profit/weight
+// sort in descending order as per profit/weight
+// start filling bag in arranged order till filled
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+class item {
+
+    public:
+        float weight;
+        float profit;
+};
+
+bool comp(item a, item b) {
+
+    float x = (a.profit)/(a.weight);
+    float y = (b.profit)/(b.weight);
+
+    return x>y;
+}
+
+int main() {
+
+    float bag = 60;
+    float max_profit=0;
+
+    item A[] = {{5,30},{10,40},{15,45},{22,77},{25,90}};
+
+    sort(A,A+5,comp);
+
+    cout<<"sorted data: ";
+
+    for(int i=0;i<5;i++) {
+        cout<<"["<<A[i].weight<<","<<A[i].profit<<"]";
+    }
+
+    cout<<endl;
+
+    for(int i=0;i<5;i++) {
+
+        if(bag<=A[i].weight) {
+
+            max_profit = max_profit + bag*(A[i].profit/A[i].weight);
+            bag = 0;
+            break;
+        }
+        else {
+
+            bag = bag - A[i].weight;
+            max_profit = max_profit + A[i].profit;
+        }
+    }
+
+    cout<<"maximum profit: "<<max_profit;
+
+    cout<<endl;
+}
